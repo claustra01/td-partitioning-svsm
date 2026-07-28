11f90da98a95f108eb0af482d63d2609439c65bd
claustra01 <shinya430.g@gmail.com>
2026-07-28 16:46:03 +0900

    chore: disable log

--- kernel/src/tdx/vmexit.rs
+++ kernel/src/tdx/vmexit.rs
@@ -116,7 +116,7 @@ fn maybe_log_vmexit(vm_id: TdpVmId, _reason: &VmExitReason) {
         .is_ok()
     {
         maybe_resolve_linux_banner(vm_id);
-        tcp_log::maybe_log_tcp_connections(vm_id);
+        // tcp_log::maybe_log_tcp_connections(vm_id);
     }
 }
 
